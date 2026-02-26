c632a5e7f82915c0bceca4d6fa6da3a782e2a5a4
Max Kaiser <max@kaiser.family>
2026-02-26 14:48:00 +0100

    finished nvim config

--- lua/max/plugins/snacks.lua
+++ lua/max/plugins/snacks.lua
@@ -154,7 +154,8 @@ return {
 
             -- Snacks Picker
             { "<leader>pf", function() require("snacks").picker.files() end, desc = "Find Files (Snacks Picker)" },
-            { "<leader>pc", function() require("snacks").picker.files({ cwd = "~/dotfiles/nvim/.config/nvim/lua" }) end, desc = "Find Config File" },
+            -- NOTE: change this depending on which OS you are on Linux or Windows
+            { "<leader>pc", function() require("snacks").picker.files({ cwd = "C:/Users/User/AppData/Local/nvim" }) end, desc = "Find Config File" },
             { "<leader>ps", function() require("snacks").picker.grep() end, desc = "Grep word" },
             { "<leader>pws", function() require("snacks").picker.grep_word() end, desc = "Search Visual selection or Word", mode = { "n", "x" } },
             { "<leader>pk", function() require("snacks").picker.keymaps({ layout = "ivy" }) end, desc = "Search Keymaps (Snacks Picker)" },
